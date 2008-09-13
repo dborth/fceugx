abb58f99b5eb87d62834265e1eadcf2fc78897d7
dborth <dborth@0aaa047c-bd49-0410-9b25-49477284f1fd>
2008-09-13 01:51:10 +0000

    delete files

--- source/fceultra/1.bat
+++
@@ -1,4 +0,0 @@
-del C:\devkitPro\devkitPPC\Fc\build\ines*.* 
-del C:\devkitPro\devkitPPC\Fc\build\mmc3*.* 
-del C:\devkitPro\devkitPPC\Fc\build\unif*.* 
-del C:\devkitPro\devkitPPC\Fc\build\simple*.* 
