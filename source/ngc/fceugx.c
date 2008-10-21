d2b8d97d4071bd7caffe9f89f7d85142a9dd12f0
dborth <dborth@0aaa047c-bd49-0410-9b25-49477284f1fd>
2008-10-21 08:08:40 +0000

    fix for Qoob modchip users

--- source/ngc/fceugx.c
+++ source/ngc/fceugx.c
@@ -94,12 +94,39 @@ void SyncSpeed()
 }
 
 /****************************************************************************
+ * ipl_set_config
+ * lowlevel Qoob Modchip disable
+ ***************************************************************************/
+
+void ipl_set_config(unsigned char c)
+{
+	volatile unsigned long* exi = (volatile unsigned long*)0xCC006800;
+	unsigned long val,addr;
+	addr=0xc0000000;
+	val = c << 24;
+	exi[0] = ((((exi[0]) & 0x405) | 256) | 48);	//select IPL
+	//write addr of IPL
+	exi[0 * 5 + 4] = addr;
+	exi[0 * 5 + 3] = ((4 - 1) << 4) | (1 << 2) | 1;
+	while (exi[0 * 5 + 3] & 1);
+	//write the ipl we want to send
+	exi[0 * 5 + 4] = val;
+	exi[0 * 5 + 3] = ((4 - 1) << 4) | (1 << 2) | 1;
+	while (exi[0 * 5 + 3] & 1);
+
+	exi[0] &= 0x405;	//deselect IPL
+}
+
+/****************************************************************************
  * main
  * This is where it all happens!
  ***************************************************************************/
 
 int main(int argc, char *argv[])
 {
+#ifdef HW_DOL
+	ipl_set_config(6); // disable Qoob modchip
+#endif
 
 #ifdef WII_DVD
 	DI_Init();	// first
