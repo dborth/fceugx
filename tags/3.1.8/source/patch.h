a42064a3bd2e09cd33fe02f34bd0cdc25eacc1a9
dborth <dborth@0aaa047c-bd49-0410-9b25-49477284f1fd>
2010-06-21 03:51:30 +0000

    [3.1.8 - June 20, 2010]
    * USB improvements
    * GameCube improvements - audio, SD Gecko, show thumbnails for saves
    * Other minor changes

---
+++ tags/3.1.8/source/patch.h
@@ -0,0 +1,19 @@
+/****************************************************************************
+ * FCE Ultra
+ * Nintendo Wii/Gamecube Port
+ *
+ * Tantric October 2008
+ *
+ * patch.h
+ *
+ * IPS/UPS/PPF patch support
+ ***************************************************************************/
+
+#ifndef PATCH_H
+#define PATCH_H
+
+#include "memfile.h"
+
+int LoadPatch(int size);
+
+#endif
