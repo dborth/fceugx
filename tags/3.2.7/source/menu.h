3a1c84ad9979953367a269dce086c1a1b8097cd8
dborth <dborth@0aaa047c-bd49-0410-9b25-49477284f1fd>
2012-01-14 22:36:51 +0000

    [3.2.7 - January 14, 2012]
    * Updated core to latest FCEUX (r2383)
    * More accurate pixel scaling (thanks eke-eke!)
    * Other minor changes

---
+++ tags/3.2.7/source/menu.h
@@ -0,0 +1,46 @@
+/****************************************************************************
+ * FCE Ultra
+ * Nintendo Wii/Gamecube Port
+ *
+ * Tantric 2008-2009
+ *
+ * menu.h
+ *
+ * Main menu flow control
+ ****************************************************************************/
+
+#ifndef _MENU_H
+#define _MENU_H
+
+bool GuiLoaded();
+void InitGUIThreads();
+void MainMenu (int menuitem);
+void ErrorPrompt(const char * msg);
+int ErrorPromptRetry(const char * msg);
+void InfoPrompt(const char * msg);
+void ShowAction (const char *msg);
+void CancelAction();
+void ShowProgress (const char *msg, int done, int total);
+void ResetText();
+
+enum
+{
+	MENU_EXIT = -1,
+	MENU_NONE,
+	MENU_SETTINGS,
+	MENU_SETTINGS_FILE,
+	MENU_SETTINGS_MENU,
+	MENU_SETTINGS_NETWORK,
+	MENU_GAMESELECTION,
+	MENU_GAME,
+	MENU_GAME_SAVE,
+	MENU_GAME_LOAD,
+	MENU_GAMESETTINGS,
+	MENU_GAMESETTINGS_MAPPINGS,
+	MENU_GAMESETTINGS_MAPPINGS_CTRL,
+	MENU_GAMESETTINGS_MAPPINGS_MAP,
+	MENU_GAMESETTINGS_VIDEO,
+	MENU_GAMESETTINGS_CHEATS
+};
+
+#endif
