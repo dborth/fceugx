238de5a5e600ae5ce2ec83b7b67605396d8dacd2
dborth <dborth@0aaa047c-bd49-0410-9b25-49477284f1fd>
2012-01-24 03:22:50 +0000

    [3.2.8 - January 23, 2012]
    * Fixed bug with flipping disk sides for FDS

---
+++ tags/3.2.8/source/cheatmgr.h
@@ -0,0 +1,16 @@
+/****************************************************************************
+ * FCE Ultra
+ * Nintendo Wii/Gamecube Port
+ *
+ * Tantric 2009
+ *
+ * cheatmgr.h
+ *
+ * Cheat handling
+ ***************************************************************************/
+
+void SetupCheats();
+bool FindGameGenie();
+void OpenGameGenie();
+
+extern int numcheats;
