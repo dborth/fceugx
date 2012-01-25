e9a0d249498061219e4c94bb6757eb5310793eb4
dborth <dborth@0aaa047c-bd49-0410-9b25-49477284f1fd>
2012-01-25 16:51:36 +0000

    finalize 3.2.9

--- trunk/source/fceugx.h
+++ trunk/source/fceugx.h
@@ -17,7 +17,7 @@
 #include "fceultra/driver.h"
 
 #define APPNAME			"FCE Ultra GX"
-#define APPVERSION		"3.2.8"
+#define APPVERSION		"3.2.9"
 #define APPFOLDER 		"fceugx"
 #define PREF_FILE_NAME	"settings.xml"
 
