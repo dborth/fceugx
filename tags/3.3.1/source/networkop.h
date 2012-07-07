c5ae452e66dc231f142f3166b9247638ada711f6
dborth <dborth@0aaa047c-bd49-0410-9b25-49477284f1fd>
2012-07-07 17:45:56 +0000

    [3.3.1 - July 7, 2012]
    * Fixed PAL support

---
+++ tags/3.3.1/source/networkop.h
@@ -0,0 +1,24 @@
+/****************************************************************************
+ * FCE Ultra
+ * Nintendo Wii/Gamecube Port
+ *
+ * Tantric December 2008
+ *
+ * networkop.h
+ *
+ * Network and SMB support routines
+ ****************************************************************************/
+
+#ifndef _NETWORKOP_H_
+#define _NETWORKOP_H_
+
+void UpdateCheck();
+bool DownloadUpdate();
+void StartNetworkThread();
+bool InitializeNetwork(bool silent);
+bool ConnectShare (bool silent);
+void CloseShare();
+
+extern bool updateFound;
+
+#endif
