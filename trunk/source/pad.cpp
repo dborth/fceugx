67162e34ab72568e737b5a669ce35b787d829160
dborth <dborth@0aaa047c-bd49-0410-9b25-49477284f1fd>
2012-10-04 00:34:08 +0000

    Fix lag with GameCube controllers

--- trunk/source/pad.cpp
+++ trunk/source/pad.cpp
@@ -186,8 +186,6 @@ void SetControllers()
  *
  * Scans pad and wpad
  ***************************************************************************/
-static int padsConnected = 0;
-static u64 prev, now;
 
 void
 UpdatePads()
@@ -195,18 +193,8 @@ UpdatePads()
 	#ifdef HW_RVL
 	WPAD_ScanPads();
 	#endif
-
-	now = gettime();
-
-	if(!padsConnected && diff_sec(prev, now) < 2)
-		return;
-
-	prev = now;
-
-	padsConnected = PAD_ScanPads();
-
-	if(!padsConnected)
-		return;
+	
+	PAD_ScanPads();
 
 	for(int i=3; i >= 0; i--)
 	{
