9fca96c7a16d0aaeccb1c0fbc6fdc2efe2ec9d90
Daryl Borth <dborth@gmail.com>
2018-08-09 17:30:48 -0600

    remove start+A+B+Z trigger to go back to emulator. just use the stick or home button.

--- source/pad.cpp
+++ source/pad.cpp
@@ -516,12 +516,7 @@ bool MenuRequested()
 	for(int i=0; i<4; i++)
 	{
 		if (
-			(userInput[i].pad.substickX < -70) ||
-			(userInput[i].pad.btns_h & PAD_BUTTON_START &&
-			userInput[i].pad.btns_h & PAD_BUTTON_A &&
-			userInput[i].pad.btns_h & PAD_BUTTON_B &&
-			userInput[i].pad.btns_h & PAD_TRIGGER_Z
-			 ) 
+			(userInput[i].pad.substickX < -70)
 			#ifdef HW_RVL
 			|| (userInput[i].wpad->btns_h & WPAD_BUTTON_HOME) ||
 			(userInput[i].wpad->btns_h & WPAD_CLASSIC_BUTTON_HOME)
