feb24e8a56ac098f2aa3753aedc4feee3f5aa00a
Daryl Borth <dborth@gmail.com>
2026-09-17 14:38:00 -0600

    fix texture flush

--- source/drivers/ogc/OgcVideoDriver.cpp
+++ source/drivers/ogc/OgcVideoDriver.cpp
@@ -496,7 +496,7 @@ void OgcImageRenderer::loadTextureData(void* texture, const uint8_t* rgba, int w
 		}
 	}
 
-	int len = (padWidth * padHeight) * 2;
+	int len = (padWidth * padHeight) * 4;
 	if (len % 32) len += (32 - len % 32);
 	DCFlushRange(dst, len);
 }
@@ -525,7 +525,7 @@ void OgcImageRenderer::fillTexture(void* texture, int width, int height, ImageRe
 		}
 	}
 
-	int len = (padWidth * padHeight) * 2;
+	int len = (padWidth * padHeight) * 4;
 	if (len % 32) len += (32 - len % 32);
 	DCFlushRange(dst, len);
 }
