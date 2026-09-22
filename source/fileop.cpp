ba5021c78228c741ee2a5d5cb789098f951afc6e
Daryl Borth <dborth@gmail.com>
2026-09-22 16:51:40 -0600

    worker queue - only stop once there's truly nothing left to run

--- source/fileop.cpp
+++ source/fileop.cpp
@@ -261,7 +261,7 @@ static void * workercallback (void *)
 		while(!workerBusy && bgCount == 0 && !workerThread.stopRequested())
 			WorkerSync().workCond.wait(WorkerSync().mutex);
 
-		if(workerThread.stopRequested())
+		if(!workerBusy && bgCount == 0 && workerThread.stopRequested())
 			break;
 
 		if(workerBusy) // something is waiting on this - always ahead of queued tasks
